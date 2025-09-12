7bf3bc7120743f95a3eab6ff83f01dde7df31f31
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-12 15:39:13 +0200

    Add files via upload

---
+++ prueba3.py
@@ -0,0 +1,15 @@
+var1=4
+
+var2=3.5
+
+var3="hola"
+
+total=var1+var2
+
+print(var1)
+
+print("el valor de la variable 1 es:",var1)
+
+print("el valor de la variable 1 es:",total,"euros")
+
+print(f"el valor de {var1} + {var2} es:",total)
---
+++ prueba4.py
@@ -0,0 +1,8 @@
+var1=int(input("introduce un numero: "))
+var2=float(input("introduce otro número: "))
+
+total=var1+var2
+
+print("el resultado es: ",total)
+
+
