6aa431be9a72bd6a459b7d2d4f5f1a037beabb05
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-16 10:45:43 +0200

    Create actividad2guiada.py

---
+++ actividad2guiada.py
@@ -0,0 +1,12 @@
+var1=int(input("introduce un número: "))
+var2=int(input("introduce otro número: "))
+
+resultadosuma=var1+var2
+resultadoresta=var1-var2
+resultadomultiplicación=var1*var2
+resultadodivisión=var1/var2
+resultadodivisiónentera=var1//var2
+resultadomódulo=var1%var2
+resultadoexponenciación=var1**var2
+
+print
