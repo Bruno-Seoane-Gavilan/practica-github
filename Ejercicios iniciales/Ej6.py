06280d63755e63fec8de422e102e1c1dfd61c2dd
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-17 09:56:18 +0200

    Create Ej6.py

---
+++ Ejercicios iniciales/Ej6.py
@@ -0,0 +1,10 @@
+#6. A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras.
+
+var1= input("Escribe la primera palabra: ")
+var2= input("Escribe la segunda palabra: ")
+var3= input("Escribe la tercera palabra: ")
+var4= input("Escribe la cuarta palabra: ")
+var5= input("Escribe la quinta palabra: ")
+
+
+print("Las palabras son las siguientes:",var5,",",var4,",",var3,",",var2,",",var1)
