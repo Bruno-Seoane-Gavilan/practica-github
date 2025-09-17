e459962885135503a27c5b905a20b53cca00ca8d
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-17 09:56:12 +0200

    Create Ej3.py

---
+++ Ejercicios iniciales/Ej3.py
@@ -0,0 +1,6 @@
+#3. Programa que pida dos números enteros y realice la suma correspondiente
+var1= int(input("Introduce un número entero: "))
+var2= int(input("Introduce otro número entero: "))
+
+total = var1 + var2
+print("La respuesta es:",total)
