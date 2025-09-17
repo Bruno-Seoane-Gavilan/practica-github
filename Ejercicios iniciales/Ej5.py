114fe5bfcec7184e4d70580512ecbc11f67412ed
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-17 09:56:15 +0200

    Create Ej5.py

---
+++ Ejercicios iniciales/Ej5.py
@@ -0,0 +1,10 @@
+#5. Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código para que entre palabra y palabra haya una coma.
+
+var1= input("Escribe la primera palabra: ")
+var2= input("Escribe la segunda palabra: ")
+var3= input("Escribe la tercera palabra: ")
+var4= input("Escribe la cuarta palabra: ")
+var5= input("Escribe la quinta palabra: ")
+
+print("Las palabras son las siguientes:",var1,",",var2,",",var3,",",var4,",",var5 )
+
