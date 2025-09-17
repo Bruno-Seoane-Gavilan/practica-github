9babaf859bb8460e417c5e8d59c66ac542c1c306
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-17 09:56:06 +0200

    Create Ej2.py

---
+++ Ejercicios iniciales/Ej2.py
@@ -0,0 +1,13 @@
+#2. Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla
+# en el siguiente orden: número entero, texto y número decimal.
+
+
+var1= int(input("Introduce un número entero: "))
+var2= input("Introduce texto: ")
+var3= float(input("Introduce un número decimal: "))
+
+
+print("el número entero es:",var1)
+print("el texto es:",var2)
+print("el número decimal es:",var3)
+
