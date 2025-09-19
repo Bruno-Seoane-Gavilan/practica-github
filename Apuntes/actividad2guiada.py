e6f899d8d2c0225a47d0287f6d95f73e9b5ad488
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-19 16:03:53 +0200

    Create actividad2guiada.py

---
+++ Apuntes/actividad2guiada.py
@@ -0,0 +1,19 @@
+var1=int(input("introduce un número: "))
+var2=int(input("introduce otro número: "))
+
+division_2_decimales=format(resultadodivisión, ".2f")
+resultadosuma=var1+var2
+resultadoresta=var1-var2
+resultadomultiplicación=var1*var2
+resultadodivisión=var1/var2
+resultadodivisiónentera=var1//var2
+resultadomódulo=var1%var2
+resultadoexponenciación=var1**var2
+
+print("El resultado de la suma es: ", resultadosuma)
+print("El resultado de la resta es: ", resultadoresta)
+print("El resultado de la multiplicación es: ", resultadomultiplicación)
+print("El resultado de la división es: ", resultadodivisión)
+print("El resultado de la división entera es: ", resultadodivisiónentera)
+print("El resultado del módulo es: ", resultadomódulo)
+print("El resultado de la exponenciación es: ", resultadoexponenciación)
