80ac41694eca8ffa5c210e92a54487f8b74ae5c0
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-19 15:50:53 +0200

    Create prueba4.py

---
+++ Apuntes/prueba4.py
@@ -0,0 +1,9 @@
+var1=int(input("introduce un numero: "))
+var2=float(input("introduce otro número: "))
+
+total=var1+var2
+
+print("el resultado es: ",total)
+
+#cambio para provar el historial de versiones
+
