f8fb80611d058e5c9c6c140a6d9a2fe33d409e06
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-19 15:50:12 +0200

    Delete prueba4.py

--- prueba4.py
+++
@@ -1,9 +0,0 @@
-var1=int(input("introduce un numero: "))
-var2=float(input("introduce otro número: "))
-
-total=var1+var2
-
-print("el resultado es: ",total)
-
-#cambio para provar el historial de versiones
-
