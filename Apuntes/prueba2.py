8f7577b3dd71970e50d5e90a24dbf255d610d4d8
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-19 15:50:57 +0200

    Create prueba2.py

---
+++ Apuntes/prueba2.py
@@ -0,0 +1,17 @@
+#variable numerica
+var1=4
+
+#variable decimal
+var2=4.5
+
+#variable de texto
+var3="hola"
+
+var_4="hola"
+
+print(var2)
+
+
+print(var3)
+
+print("la variable uno tiene el valor",var1)
