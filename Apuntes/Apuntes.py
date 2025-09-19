8f089be044f3351da8567972414f0443a15c0f14
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-19 16:03:51 +0200

    Create Apuntes.py

---
+++ Apuntes/Apuntes.py
@@ -0,0 +1,37 @@
+
+#división con 2 decimales 
+division_2_decimales=format(división, ".2f")
+
+#Para redonder a 2 decimales
+var1=round(división, 2)
+
+
+#Variable numérica
+var1=4
+#Variable decimal
+var2=4.5
+#Variable de texto
+var3="hola"
+
+
+#Print sirve para mostrar información por pantalla
+print("El resultado el número es: ", var1)
+
+
+#Int sirve para convertir una variable a número entero
+var1=int(input("introduce un número: "))
+#Float sirve para convertir una variable a número decimal
+var2=float(input("introduce otro número: "))
+#Input sirve para pedir información al usuario
+var3=input("introduce un texto: ")
+
+
+#Operadores aritméticos
+suma=var1+var2
+resta=var1-var2
+multiplicación=var1*var2
+división=var1/var2
+divisiónentera=var1//var2
+módulo=var1%var2
+exponenciación=var1**var2
+print("El resultado de la suma es: ", suma)
