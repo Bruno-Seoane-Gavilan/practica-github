be49cddf5e0f5016b56adbf3e39a3bfc4fb8aec1
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-09-19 16:03:44 +0200

    Create Ej8.py

---
+++ Ejercicios iniciales/Ej8.py
@@ -0,0 +1,6 @@
+#8. programa que pida un número de horas y muestre por pantalla los minutos y segundos
+
+horas=int(input("introduce un número de horas:"))
+minutos=horas*60
+segundos=minutos*60
+print("El número de minutos es:", minutos,"y", "el número de segundos es:", segundos)
