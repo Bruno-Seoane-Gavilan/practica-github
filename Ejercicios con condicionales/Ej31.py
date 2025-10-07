ba72da970d86e699ffb6ef8713f5d314bb48a169
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-10-07 10:46:14 +0200

    Update Ej31.py

--- Ejercicios con condicionales/Ej31.py
+++ Ejercicios con condicionales/Ej31.py
@@ -8,7 +8,7 @@ palabra2="Dios"
 palabra3="quién"
 palabra4="madruga"
 palabra5="ayuda"
-if palabra==palabra1 or palabra2 or palabra3 or palabra4 or palabra5:
+if palabra==palabra1 or palabra==palabra2 or palabra==palabra3 or palabra==palabra4 or palabra==palabra5:
     print("La palabra está en la frase y su posición es: ",frase.index(palabra))
 else:
     print("La palabra no está en la frase")
