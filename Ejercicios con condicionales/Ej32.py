f3dd7e38879bc8eca35a6ea2ba7118d6081e1d12
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-10-08 09:54:38 +0200

    Update Ej32.py

--- Ejercicios con condicionales/Ej32.py
+++ Ejercicios con condicionales/Ej32.py
@@ -2,13 +2,15 @@
 
 frase="A quién madruga Dios ayuda"
 palabra=input("Introduce una palabra: ")
+palabra=palabra.lower()
+frase=frase.lower()
 
-palabra1="A"
-palabra2="Dios"
+palabra1="a"
+palabra2="dios"
 palabra3="quién"
 palabra4="madruga"
 palabra5="ayuda"
-if palabra==palabra1 or palabra2 or palabra3 or palabra4 or palabra5:
+if palabra==palabra1 or palabra==palabra2 or palabra==palabra3 or palabra==palabra4 or palabra==palabra5:
     print("La palabra está en la frase y su posición es: ",frase.index(palabra))
 else:
     print("La palabra no está en la frase")
