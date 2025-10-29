3c556a202b7b10d3b02f9364184f33eefda46717
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-10-29 09:54:53 +0100

    Create Codigo.py

---
+++ Proyecto Password/Codigo.py
@@ -0,0 +1,34 @@
+print("Introduce una contraseña de entre 6 y 8 caracteres que cumpla con lo siguiente:")
+
+print("Posición 1: Un número mayor o igual 1 y menor o igual que 5")
+print("Posición 2: Una letra minúscula")
+print("Posición 3: Una letra mayúscula")
+print("Posición 4: Uno de los siguientes simbolos *, _")
+print("Posición 5: Una letra minúscula")
+print("Posición 6: Un número mayor o igual 6 y menor o igual que 9")
+print("Posición 7: Uno de los siguientes simbolos &,/, #")
+print("Posición 8: Un número menor o igual que 5")
+
+password=input("Introduce la contraseña:")
+
+longitud_password=len(password)
+
+if longitud_password <=5 or longitud_password>= 9:
+    print("Error, el password tiene una longitud de",longitud_password,"que no es la que se ha pedido")
+else:
+    if password isnumeric
+    if int(password[0])<=1 or int(password[0])>=5:
+        print("Error en el caracter 1")
+    if not (password[1].islower):
+        print("Error en el caracter 2")
+    if (password[2].islower):
+        print("Error en el caracter 3")
+    if not (password[3]=="*" or password[3]=="_"):
+        print("Error en el caracter 4")
+    if not (password[4].islower):
+        print("Error en el caracter 5")
+    if
+
+
+    
+
