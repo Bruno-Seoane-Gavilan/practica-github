f2865db063c9b05943e2a8716e16078ad963d28c
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-11-07 16:04:40 +0100

    Update Ej35.py

--- Ejercicios con bucles/Ej35.py
+++ Ejercicios con bucles/Ej35.py
@@ -1,6 +1,6 @@
 #35. Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre
 
-repeticiones=input("Introduce el número de veces que quieres que se repita tu nombre: ")
+repeticiones=int(input("Introduce el número de veces que quieres que se repita tu nombre: "))
 
-for repeticiones in range():
+for j in range(repeticiones):
     print("Bruno Seoane Gavilán")
