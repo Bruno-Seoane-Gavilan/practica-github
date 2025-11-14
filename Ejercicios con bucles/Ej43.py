487e01a684331cb0d412731bea3271d487aa1a02
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-11-14 15:53:54 +0100

    Update Ej43.py

--- Ejercicios con bucles/Ej43.py
+++ Ejercicios con bucles/Ej43.py
@@ -1,6 +1,6 @@
 #43. Realiza un programa que recorra con un for una palabra introducida por teclado y se imprima por pantalla cada letra
 
 palabra=input("Introduce una palabra: ")
-for j in range len((palabra)):
-    print("palabra"[j])
+for j in range (len((palabra))):
+    print("En la posición",j,"está la ",palabra[j])
      
