522b395f7f1ac1555a39ab6ae030f8c0ab762f7f
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-11-26 09:55:40 +0100

    Create Ejercicio2.py

---
+++ Simulacro trimestral/Ejercicio2.py
@@ -0,0 +1,15 @@
+suma_positivos=0
+suma_negativos=0
+contador_cien=0
+
+for x in range(7):
+    numero=int(input("Introduce en número: "))
+    if numero>0:
+        suma_positivos=suma_positivos+numero
+        if numero>100:
+            contador_cien +=1
+    else:
+        suma_negativos=suma_negativos+numero
+print("Suma de positivos:", suma_positivos)
+print("Suma de negativos:", suma_negativos)
+print("Números mayores de 100: ",contador_cien)
