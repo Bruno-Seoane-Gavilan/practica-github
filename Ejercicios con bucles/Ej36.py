d30b69b28d3d75aba08dcadbd3273cd309d14a83
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-11-26 09:54:05 +0100

    Delete Ej36.py

--- Ejercicios con bucles/Ej36.py
+++
@@ -1,9 +0,0 @@
-#36. Programa que sume los n primeros números naturales. n Lo introduce el usuario.
-
-numeros_naturales=int(input("Introduce el número de primeros números naturales quieres sumar: "))
-numero=1
-total=0
-for j in range(numeros_naturales):
-    total=total+numero
-    numero=numero+1
-print("La suma total de números naturales es: ",total)
