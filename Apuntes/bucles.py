ea748805cb2ed80503048a53d33beb9618553784
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2025-11-29 21:26:55 +0100

    Create bucles.py

---
+++ Apuntes/bucles.py
@@ -0,0 +1,8 @@
+#for (sabemos el numero de veces que se tiene que repetir)
+for j in range (0,10):
+
+for j in range (0,10,2) # saltando del 0 al 10 de 2 en 2
+
+for j in string: 
+
+#while + condición (el usuario determina si quiere salir del bucle o seguir)
