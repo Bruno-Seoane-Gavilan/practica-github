6c8600f3351233fb0f0daadefca80d6fd8e982a1
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2026-01-30 15:58:31 +0100

    0

--- Proyecto DNI/Proyecto.py
+++ Proyecto DNI/Proyecto.py
@@ -1,19 +1,21 @@
-dni=0
+dni=""
+dni_resultante=[]
 repetir="s"
+letras_posibles_dni="TRWAGMYFPDXBNJZSQVHLCKE"
+letra_dni=""
 
 while repetir=="s":
-    dni=int(input("Introduce un DNI: "))
+    dni=(input("Introduce un DNI: "))
     dni_string=str(dni)
     if not len(dni_string)==8:
         print("El DNI no tiene la longitud correcta")
-    if type(dni)==int:
+    if not dni.isdigit():
         print("El DNI tiene que ser solo números")
     else:
-
-
+        resto_dni=int(dni)%23
+        letra_dni=letras_posibles_dni[resto_dni]
+        dni_resultante.append(dni)
+        dni_resultante.append(letra_dni)
+        print(dni,"-",letra_dni)
+    repetir=input("¿Quieres volver a introducir otro DNI? s/n: ")
         
-
-
-
-        
-
