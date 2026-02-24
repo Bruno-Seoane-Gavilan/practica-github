2773487b685d4dc608ad31f752c23f30b559352a
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2026-02-24 10:44:54 +0100

    2

--- Ahorcado/Ahorcado.py
+++ Ahorcado/Ahorcado.py
@@ -2,6 +2,7 @@ lista_palabrasecreta=["albahaca","regodeo","oleaginoso","barbilampiño","joyel",
 lista_partida=[]
 lista_ahorcado=[]
 letra=""
+repetir=""
 import random
 palabra_secreta=random.choice(lista_palabrasecreta)
 print(palabra_secreta)
@@ -16,4 +17,16 @@ if letra in palabra_secreta:
             lista_partida.append(letra)
         else:
             lista_partida.append("_")
+else:
+    print("La letra no está en la palabra")
 print(*lista_partida)
+while "_" in lista_partida:
+    letra=input("Introduce otra letra: ")
+    if letra in palabra_secreta:
+        for x in range(len(palabra_secreta)):
+            if palabra_secreta[x]==letra:
+                lista_partida[x]=letra
+    elif letra not in palabra_secreta:
+        print("La letra no está en la palabra")
+    print(*lista_partida)
+print("¡Has ganado!")
