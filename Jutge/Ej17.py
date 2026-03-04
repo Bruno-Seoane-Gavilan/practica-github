afbc3d024a8e60a7ecafd297cf65020df0491d24
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2026-03-04 09:52:30 +0100

    2

--- Jutge/Ej17.py
+++ Jutge/Ej17.py
@@ -1,4 +1,6 @@
 year=int(input())
 if year%4==0 and not(year%100==0) or (year%100==0 and year%400==0):
+#Aqui se hace la división para combrobar se es bisiesto(divisible entre 4 y no entre 100) o no lo es.
     print("YES")
-else:    print("NO")
+else:
+    print("NO")
