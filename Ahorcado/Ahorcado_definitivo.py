2daa711909ad3a8462c29a4440a86495cb8167d4
Bruno-Seoane-Gavilan <bruno.seoane@campus.xaloc.org>
2026-03-06 16:02:51 +0100

    3

--- Ahorcado/Ahorcado_definitivo.py
+++ Ahorcado/Ahorcado_definitivo.py
@@ -8,68 +8,62 @@ lista_errores=[]
 lista_letrarepetida=[]
 cronometro="s"
 
-while repetir in ["si","sí", "s", "S", "Si", "SI"]:
-    lista_partida=[]
-    import random
-    cronometro=input("¿Quieres activar el cronómetro? (si/no): ")
-    while cronometro in ["si","sí", "s", "S", "Si", "SI"]:
-        import time
-        tiempo_inicio=time.time()
-        palabra_secreta=random.choice(lista_palabrasecreta)
-        tiempo_final=time.time()
-        tiempo_total=tiempo_final-tiempo_inicio
+import random
+cronometro=input("¿Quieres activar el cronómetro? (si/no): ")
+while cronometro in ["si","sí", "s", "S", "Si", "SI"]:
+    import time
+    tiempo_inicio=time.time()
     palabra_secreta=random.choice(lista_palabrasecreta)
-    print(palabra_secreta)
-    lista_palabrasecreta.remove(palabra_secreta)
+    tiempo_final=time.time()
+    tiempo_total=tiempo_final-tiempo_inicio
+    redondear_tiempo=round(tiempo_total,3)
 
-    for x in palabra_secreta:
-        lista_partida.append("_")
-    print(*lista_partida)
+    while repetir in ["si","sí", "s", "S", "Si", "SI"]:
+        palabra_secreta=random.choice(lista_palabrasecreta)
+        print(palabra_secreta)
+        lista_palabrasecreta.remove(palabra_secreta)
 
-    while "_" in lista_partida and errores<8:
-        letra=input("Introduce una letra: ")
-        if letra in palabra_secreta:
-            for x in range(len(palabra_secreta)):
-                if palabra_secreta[x]==letra:
-                    lista_partida[x]=letra
-            lista_aciertos.append(letra)
-            num_aciertos=len(lista_aciertos)
-        else:
-            errores=errores+1
-            lista_errores.append(letra)
-            num_errores=len(lista_errores)
-            print("La letra que has introducido no esta en la palabra")
-            print("".join(lista_ahorcado[:errores]))
+        for x in palabra_secreta:
+            lista_partida.append("_")
         print(*lista_partida)
-        if "_" not in lista_partida:
-            print("¡Has ganado!")
-            print("Has acertado",num_aciertos,"letras")
-            print("Has fallado",num_errores,"veces")
-            print("Has tardado",tiempo_total,"segundos en adivinar la palabra")
+
+        while "_" in lista_partida and errores<8:
+            letra=input("Introduce una letra: ")
+            if letra in palabra_secreta:
+                for x in range(len(palabra_secreta)):
+                    if palabra_secreta[x]==letra:
+                        lista_partida[x]=letra
+                lista_aciertos.append(letra)
+                num_aciertos=len(lista_aciertos)
+            else:
+                print("La letra que has introducido no esta en la palabra")
+                print("".join(lista_ahorcado[:errores]))
+                lista_errores.append(letra)
+                num_errores=len(lista_errores)
+            print(*lista_partida)
+            if "_" not in lista_partida:
+                print("¡Has ganado!")
+                print("Has acertado",num_aciertos,"letras")
+                print("Has fallado",num_errores,"veces")
+                print("Has tardado",redondear_tiempo,"segundos en adivinar la palabra")
+                nuevas_palabras=input("¿Quieres añadir nuevas palabras al juego? (si/no): ")
+                if nuevas_palabras in ["si","sí", "s", "S", "Si", "SI"]:
+                    while nuevas_palabras in ["si","sí", "s", "S", "Si", "SI"]:
+                        nueva_palabra=input("Introduce la nueva palabra: ")
+                        lista_palabrasecreta.append(nueva_palabra)
+                        nuevas_palabras=input("¿Quieres añadir otra palabra? (si/no): ")
+                repetir=input("¿Quieres jugar otra vez? (si/no): ")
+                if repetir not in ["si","sí", "s", "S", "Si", "SI"]:
+                    print("¡Hasta la próxima!")
+        if errores==8:
             nuevas_palabras=input("¿Quieres añadir nuevas palabras al juego? (si/no): ")
             if nuevas_palabras in ["si","sí", "s", "S", "Si", "SI"]:
                 while nuevas_palabras in ["si","sí", "s", "S", "Si", "SI"]:
                     nueva_palabra=input("Introduce la nueva palabra: ")
                     lista_palabrasecreta.append(nueva_palabra)
                     nuevas_palabras=input("¿Quieres añadir otra palabra? (si/no): ")
+                repetir=input("¿Quieres jugar otra vez? (si/no): ")
+            print("¡Has perdido!, la palabra secreta era",palabra_secreta)
             repetir=input("¿Quieres jugar otra vez? (si/no): ")
             if repetir not in ["si","sí", "s", "S", "Si", "SI"]:
-                print("Has acertado",num_aciertos,"letras")
-                print("Has fallado",num_errores,"veces")
-                print("Has tardado",tiempo_total,"segundos en adivinar la palabra")
                 print("¡Hasta la próxima!")
-    if errores==8:
-        nuevas_palabras=input("¿Quieres añadir nuevas palabras al juego? (si/no): ")
-        if nuevas_palabras in ["si","sí", "s", "S", "Si", "SI"]:
-            while nuevas_palabras in ["si","sí", "s", "S", "Si", "SI"]:
-                nueva_palabra=input("Introduce la nueva palabra: ")
-                lista_palabrasecreta.append(nueva_palabra)
-                nuevas_palabras=input("¿Quieres añadir otra palabra? (si/no): ")
-            repetir=input("¿Quieres jugar otra vez? (si/no): ")
-        print("¡Has perdido!, la palabra secreta era",palabra_secreta)
-        repetir=input("¿Quieres jugar otra vez? (si/no): ")
-        if repetir not in ["si","sí", "s", "S", "Si", "SI"]:
-            print("Has acertado",num_aciertos,"letras")
-            print("Has fallado",num_errores,"veces")
-            print("Has tardado",tiempo_total,"segundos en adivinar la palabra")
-            print("¡Hasta la próxima!")
